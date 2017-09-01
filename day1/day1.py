fbd41479a4ad459b035a71e535affc56a4343eca
Henrique <henrique@localhost.localdomain>
2017-08-31 23:44:27 -0300

    [DAY-1] Search first location you visit twice

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,24 @@
+movement_history = [[0, 0]];
+isStillSearching = True;
+
+def addAndCheckTwiceVisit(arrayToAdd) :
+    global isStillSearching;
+    if (isStillSearching and arrayToAdd in movement_history) :
+        isStillSearching = False;
+        distance = abs(arrayToAdd[0]) + abs(arrayToAdd[1]);
+        print "You visited twice:";
+        print "...... x = " + str(arrayToAdd[0]);
+        print "...... y = " + str(arrayToAdd[1]);
+        print "The HQ is " + str(distance) + " blocks away.";
+
+    movement_history.append(arrayToAdd);
+
+
+
 def calcTaxiCabGeometryFrom(filePath):
-    degrees = 0;
     coordinate_x = 0;
     coordinate_y = 0;
+    degrees = 0;
 
     input = open(str(filePath), "r").read();
 
@@ -12,19 +29,31 @@ def calcTaxiCabGeometryFrom(filePath):
             degrees += -90;
 
         module = degrees % 360;
-
         steps = int(movement[1:]);
 
+        count = 0;
         if (module == 0):
+            while count < steps :
+                count += 1;
+                addAndCheckTwiceVisit([coordinate_x, coordinate_y + count])
             coordinate_y += steps;
         elif (module == 90 or module == -270):
+            while count < steps :
+                count += 1;
+                addAndCheckTwiceVisit([coordinate_x + count, coordinate_y])
             coordinate_x += steps;
         elif (abs(module) == 180):
+            while count < steps :
+                count += 1;
+                addAndCheckTwiceVisit([coordinate_x, coordinate_y - count])
             coordinate_y -= steps;
         elif (module == 270 or module == -90):
+            while count < steps :
+                count += 1;
+                addAndCheckTwiceVisit([coordinate_x - count, coordinate_y])
             coordinate_x -= steps;
 
     distance = abs(coordinate_x) + abs(coordinate_y);
-    print "The distance is " + str(distance);
+    print "The final path is " + str(distance) + " blocks away.";
 
 if __name__ == '__main__': calcTaxiCabGeometryFrom("input.txt")
