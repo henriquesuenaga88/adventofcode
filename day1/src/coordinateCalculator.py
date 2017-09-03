71ff1b0b267cf0954e1b3f27ce7342b9154f5ede
Henrique <henrique@localhost.localdomain>
2017-09-03 00:23:46 -0300

    [DAY-1] - removing 'input' inicialization\n - Code Standardization\n - Switch Case implementation\n - Added venv

--- day1/src/coordinateCalculator.py
+++ day1/src/coordinateCalculator.py
@@ -6,25 +6,24 @@ class CoordinateCalculator:
     global is_still_searching
     is_still_searching = True
 
-    input = ""
     total_distance = 0
-    firstLocationVisitedTwiceDistance = 0
+    first_location_visited_twice_distance = 0
 
     def __init__(self, instructions):
         self.input = instructions
 
 
-    def addAndCheckTwiceVisit(self, array_to_add):
+    def add_and_check_twice_visit(self, array_to_add):
         global movement_history
         global is_still_searching
         if (is_still_searching and array_to_add in movement_history):
             is_still_searching = False
             coordinates_x, coordinates_y = array_to_add
-            self.firstLocationVisitedTwiceDistance = abs(coordinates_x + coordinates_y)
+            self.first_location_visited_twice_distance = abs(coordinates_x + coordinates_y)
 
         movement_history.append(array_to_add)
 
-    def calcTaxiCabGeometryFrom(self):
+    def calc_taxicab_geometry_from(self):
         coordinate_x = 0
         coordinate_y = 0
         degrees = 0
@@ -38,27 +37,38 @@ class CoordinateCalculator:
             module = degrees % 360
             steps = int(movement[1:])
 
-            count = 0
-            if (module == 0):
-                while count < steps:
-                    count += 1
-                    self.addAndCheckTwiceVisit([coordinate_x, coordinate_y + count])
-                coordinate_y += steps
-            elif (module == 90 or module == -270):
-                while count < steps:
-                    count += 1
-                    self.addAndCheckTwiceVisit([coordinate_x + count, coordinate_y])
-                coordinate_x += steps
-            elif (abs(module) == 180):
-                while count < steps:
-                    count += 1
-                    self.addAndCheckTwiceVisit([coordinate_x, coordinate_y - count])
-                coordinate_y -= steps
-            elif (module == 270 or module == -90):
-                while count < steps:
-                    count += 1
-                    self.addAndCheckTwiceVisit([coordinate_x - count, coordinate_y])
-                coordinate_x -= steps
+            while switch(module):
+                count = 0
+                if case(0): #North
+                    while count < steps:
+                        count += 1
+                        self.add_and_check_twice_visit([coordinate_x, coordinate_y + count])
+                    coordinate_y += steps
+                if case(90, -270): #East
+                    while count < steps:
+                        count += 1
+                        self.add_and_check_twice_visit([coordinate_x + count, coordinate_y])
+                    coordinate_x += steps
+                if case(180, -180):#South
+                    while count < steps:
+                        count += 1
+                        self.add_and_check_twice_visit([coordinate_x, coordinate_y - count])
+                    coordinate_y -= steps
+                if case(270, -90):#West
+                    while count < steps:
+                        count += 1
+                        self.add_and_check_twice_visit([coordinate_x - count, coordinate_y])
+                    coordinate_x -= steps
+                break
 
         self.total_distance = abs(coordinate_x) + abs(coordinate_y)
         return self
+
+class switch(object):
+    value = None
+    def __new__(class_, value):
+        class_.value = value
+        return True
+
+def case(*args):
+    return any((arg == switch.value for arg in args))
--- day1/tests/coordinateCoordinatorTest.py
+++ day1/tests/coordinateCoordinatorTest.py
@@ -3,26 +3,25 @@ from ..src.coordinateCalculator import CoordinateCalculator
 
 class TestDay1(unittest.TestCase):
 
-    def test_calcTaxiCabGeometryFromR2L3(self):
-        calculator = CoordinateCalculator("R2, L3")
-        calculator.calcTaxiCabGeometryFrom()
+    def test_calc_taxicab_geometry_from_R2_L3(self):
+        calculator = CoordinateCalculator("R2, L3").calc_taxicab_geometry_from()
         self.assertEqual(calculator.total_distance, 5)
-        self.assertEqual(calculator.firstLocationVisitedTwiceDistance, 0)
+        self.assertEqual(calculator.first_location_visited_twice_distance, 0)
 
-    def test_calcTaxiCabGeometryFromR2R2R2(self):
-        calculator = CoordinateCalculator("R2, R2, R2").calcTaxiCabGeometryFrom()
+    def test_calc_taxicab_geometry_from_R2_R2_R2(self):
+        calculator = CoordinateCalculator("R2, R2, R2").calc_taxicab_geometry_from()
         self.assertEqual(calculator.total_distance, 2)
-        self.assertEqual(calculator.firstLocationVisitedTwiceDistance, 1)
+        self.assertEqual(calculator.first_location_visited_twice_distance, 1)
 
-    def test_calcTaxiCabGeometryFromR5L5R5R3(self):
-        calculator = CoordinateCalculator("R5, L5, R5, R3").calcTaxiCabGeometryFrom()
+    def test_calc_taxicab_geometry_from_R5_L5_R5_R3(self):
+        calculator = CoordinateCalculator("R5, L5, R5, R3").calc_taxicab_geometry_from()
         self.assertEqual(calculator.total_distance, 12)
-        self.assertEqual(calculator.firstLocationVisitedTwiceDistance, 0)
+        self.assertEqual(calculator.first_location_visited_twice_distance, 0)
 
-    def test_calcTaxiCabGeometryFromR8R4R4R8(self):
-        calculator = CoordinateCalculator("R8, R4, R4, R8").calcTaxiCabGeometryFrom()
+    def test_calc_taxicab_geometry_from_R8_R4_R4_R8(self):
+        calculator = CoordinateCalculator("R8, R4, R4, R8").calc_taxicab_geometry_from()
         self.assertEqual(calculator.total_distance, 8)
-        self.assertEqual(calculator.firstLocationVisitedTwiceDistance, 4)
+        self.assertEqual(calculator.first_location_visited_twice_distance, 4)
 
 if __name__ == '__main__':
     unittest.main()
